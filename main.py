d272d889b91a15cd56f66267c01d617be3d06c59
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-26 06:55:19 +0000

    Add user prompts

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 """
 A secret auction application
 """
+
+from logo import logo
+
+# Default displays
+print(logo)
+print('Each bids will be hidden')
+
+
+# Keep the different bids in a dictionary
+bids = {}
+
+# User prompts
+name = input("What is your name?\n").title()
+price = int(input("How much are you bidding?\n€"))
+
+# Add the input to the dictionary
+bids[name] = price
