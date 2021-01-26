c7d4ec5f8360e4a7af5f9a76a63120bc782d6f50
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-26 06:57:58 +0000

    Add another prompt

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ A secret auction application
 """
 
 from logo import logo
+import os
 
 # Default displays
 print(logo)
 print('Each bids will be hidden')
+
+
+# To prevent bidders from seeing bids different from theirs
+def clear():
+    os.system('clear')
 
 
 # Keep the different bids in a dictionary
@@ -18,3 +24,14 @@ price = int(input("How much are you bidding?\n€"))
 
 # Add the input to the dictionary
 bids[name] = price
+
+# Prompt for another user
+another_bid = input("Is there a new bid? Type 'Yes' or 'No'. \n").title()
+
+# Verify the input
+if another_bid == 'Yes':
+    clear()
+elif another_bid == 'No':
+    print('NO more')
+else:
+    print("Invalid Input")
